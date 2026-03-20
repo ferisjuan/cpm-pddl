69ca8cef0a4804af95455c21d5efe6e3fc75920a
Juan Feris <feris.juan@gmail.com>
2026-03-20 15:54:00 -0500

    feat: fix file tree

--- ftplugin/pddl.lua
+++ ftplugin/pddl.lua
@@ -1,15 +1,17 @@
 -- ftplugin/pddl.lua
 -- Buffer-local settings for PDDL files
 
-if vim.b.did_ftplugin_pddl then return end
+if vim.b.did_ftplugin_pddl then
+	return
+end
 vim.b.did_ftplugin_pddl = true
 
 local opt = vim.opt_local
 
 -- PDDL uses Lisp-style indentation
-opt.expandtab   = true
-opt.shiftwidth  = 2
-opt.tabstop     = 2
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
 opt.softtabstop = 2
 
 -- Treat '-' as part of a word (common in PDDL identifiers like :durative-action)
@@ -20,10 +22,10 @@ opt.commentstring = "; %s"
 
 -- Fold on parentheses
 opt.foldmethod = "syntax"
-opt.foldlevel  = 99   -- open all folds by default
+opt.foldlevel = 99 -- open all folds by default
 
 -- Match parens for bracket highlighting
-opt.showmatch  = true
+opt.showmatch = true
 
 -- Auto-pair helpers (if user has no dedicated plugin, minimal help)
 vim.keymap.set("i", "(", "()<Left>", { buffer = true, desc = "Auto-close paren" })
